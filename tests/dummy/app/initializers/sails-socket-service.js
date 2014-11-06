eede63a03d800942d950b9ef5e8c4b7a0a1e1da3
Huafu Gandon <huafu.gandon@gmail.com>
2014-11-06 16:00:10 +0700

    Removed the initializer from the dummy app

--- tests/dummy/app/initializers/sails-socket-service.js
+++
@@ -1,5 +0,0 @@
-import SailsSocketServiceInitializer from 'ember-data-sails/initializers/ember-data-sails';
-import {initialize} from 'ember-data-sails/initializers/ember-data-sails';
-
-export var initialize;
-export default SailsSocketServiceInitializer;
